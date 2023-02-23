33fb506b8596faf866c16569a02bfd2b07f47a24
Vitalii Popov vypop12@mts.ru <popoffvg@gmail.com>
2023-02-23 15:26:39 +0400

    add command in errror

--- jq.go
+++ jq.go
@@ -1,6 +1,7 @@
 package jq
 
 import (
+	"errors"
 	"fmt"
 	"os/exec"
 )
@@ -9,6 +10,10 @@ func ExecForPath(path, expression string) (string, error) {
 	const cmd = "cat %s | jq %s"
 	out, err := exec.Command("bash", "-c", fmt.Sprintf(cmd, path, expression)).Output()
 	if err != nil {
+		var execErr exec.ExitError
+		if errors.As(err, &execErr) {
+			return "", fmt.Errorf("%w: %s %s", err, cmd, string(execErr.Stderr))
+		}
 		return "", fmt.Errorf("%w: %s %s", err, cmd, string(out))
 	}
 
