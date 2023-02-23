6bcca01eebb20ad0f1359503543a3c5bd166312c
Vitalii Popov vypop12@mts.ru <popoffvg@gmail.com>
2023-02-23 15:32:39 +0400

    add command in errror

--- jq.go
+++ jq.go
@@ -10,7 +10,7 @@ func ExecForPath(path, expression string) (string, error) {
 	const cmd = "cat %s | jq %s"
 	out, err := exec.Command("bash", "-c", fmt.Sprintf(cmd, path, expression)).Output()
 	if err != nil {
-		var execErr exec.ExitError
+		var execErr *exec.ExitError
 		if errors.As(err, &execErr) {
 			return "", fmt.Errorf("%w: %s %s", err, cmd, string(execErr.Stderr))
 		}
